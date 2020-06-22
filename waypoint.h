3892071792b0a93051052c2c74b5aabc0e023f6e
Talath9801 <1098454874@qq.com>
2020-06-22 19:01:06 +0800

    size, add tower right

---
+++ waypoint.h
@@ -0,0 +1,23 @@
+#ifndef WAYPOINT_H
+#define WAYPOINT_H
+
+#include <QPoint>
+
+class QPainter;
+
+// 敌人移动的航线
+class WayPoint
+{
+public:
+	WayPoint(QPoint pos);
+
+	void setNextWayPoint(WayPoint *nextPoint);
+	WayPoint* nextWayPoint() const;
+	const QPoint pos() const;
+
+private:
+	const QPoint		m_pos;
+	WayPoint *			m_nextWayPoint;
+};
+
+#endif // WAYPOINT_H
