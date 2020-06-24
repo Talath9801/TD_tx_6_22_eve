7f39130230d8ab6b581655950735f45db9839c03
Talath9801 <1098454874@qq.com>
2020-06-24 18:52:31 +0800

    改图片名称

---
+++ enemydouble.cpp
@@ -0,0 +1,8 @@
+#include "enemydouble.h"
+
+
+EnemyDouble::EnemyDouble(WayPoint *startWayPoint, MainWindow *game, const QPixmap &sprite)
+    :Enemy (startWayPoint,game,sprite)
+{
+
+}
---
+++ enemydouble.h
@@ -0,0 +1,11 @@
+#ifndef ENEMYDOUBLE_H
+#define ENEMYDOUBLE_H
+
+#include <enemy.h>
+class EnemyDouble:public Enemy
+{
+public:
+    EnemyDouble(WayPoint *startWayPoint, MainWindow *game, const QPixmap &sprite = QPixmap(":/image/enemydouble.png"));
+};
+
+#endif // ENEMYDOUBLE_H
